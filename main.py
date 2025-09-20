0e094640ee1549a73870bb5a28d28d8fbc3e4055
Mitchell Lipnitsky <jediminor751@gmail.com>
2025-09-19 20:23:33 -0400

    top5 films feature added backend

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy #lets use use the MySQL db
+from sqlalchemy import text #allows for SQL queries
+from flask_cors import CORS #lets us communicate with react
+from dotenv import load_dotenv, find_dotenv #for env file used to run backend
+import os #used to read env variables
+
+load_dotenv(find_dotenv()) #connection to mySQL db in env
+
+app = Flask(__name__) #creating flask application object
+
+CORS(app, resources={
+    r"/api/*": {
+        "origins": [ #the ports that flask will be able to connect to
+            "http://localhost:5173", 
+            "http://127.0.0.1:5173",
+            "http://localhost:3000", #3000 initially used for tutorial and 3001 for actual project
+            "http://127.0.0.1:3000",
+            "http://localhost:3001",
+            "http://127.0.0.1:3001",
+            ]}})
+
+db_uri = os.getenv("DATABASE_URL") #getting the db connection from env
+app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app) #db object
+
+@app.get("/api/home") #route to homepage
+def top5_films(): #function for getting the top 5 films, used query written for milestone 1 question 6
+    sql = text("""
+        SELECT f.film_id, f.title, COUNT(r.rental_id) AS rentals_count
+        FROM film f
+        JOIN inventory i ON i.film_id = f.film_id
+        JOIN rental   r ON r.inventory_id = i.inventory_id
+        GROUP BY f.film_id, f.title
+        ORDER BY rentals_count DESC
+        LIMIT 5;
+    """)
+    with db.engine.connect() as conn: #connecting to the db
+        rows = conn.execute(sql).mappings().all() #executing the query
+    return jsonify([dict(r) for r in rows]) #getting the rows and putting them into a dictionary
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True) #running and restarting the server if changes are made on port 127.0.0.1
