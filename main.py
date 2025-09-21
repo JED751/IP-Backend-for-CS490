156b3b324b5e8e428a5dbd841a068795e8ceca12
Mitchell Lipnitsky <jediminor751@gmail.com>
2025-09-21 16:28:51 -0400

    backend: 4/14 click on an actor and see details and top 5 rented films added

--- main.py
+++ main.py
@@ -27,15 +27,15 @@ db = SQLAlchemy(app) #db object
 
 #As a user I want to view top 5 rented films of all time
 @app.get("/api/films/top5") #route to homepage
-def top5_films(): #function for getting the top 5 films, used query written for milestone 1 question 6
+def top5_films(): #function for getting the top 5 films
     sql = text("""
-               SELECT f.film_id, f.title, COUNT(r.rental_id) AS rentals_count
-               FROM film AS f
-               JOIN inventory i ON i.film_id = f.film_id
-               JOIN rental r ON r.inventory_id = i.inventory_id
-               GROUP BY f.film_id, f.title
-               ORDER BY rentals_count DESC
-               LIMIT 5;
+        SELECT f.film_id, f.title, COUNT(r.rental_id) AS rentals_count
+        FROM film AS f
+        JOIN inventory i ON i.film_id = f.film_id
+        JOIN rental r ON r.inventory_id = i.inventory_id
+        GROUP BY f.film_id, f.title
+        ORDER BY rentals_count DESC
+        LIMIT 5;
     """)
     with db.engine.connect() as conn: #connecting to the db
         rows = conn.execute(sql).mappings().all() #executing the query
@@ -43,14 +43,14 @@ def top5_films(): #function for getting the top 5 films, used query written for 
 
 #As a user I want to be able to view top 5 actors that are part of films I have in the store
 @app.get("/api/actors/top5")
-def top5_actors():
+def top5_actors(): #function for getting the top 5 actors based on movie count
     sql = text("""                     
-               SELECT a.actor_id, CONCAT(a.first_name, ' ', a.last_name) AS name, COUNT(DISTINCT fa.film_id) AS films_count
-               FROM actor AS a
-               JOIN film_actor fa ON fa.actor_id = a.actor_id
-               GROUP BY a.actor_id, name
-               ORDER BY films_count DESC, name ASC
-               LIMIT 5;
+        SELECT a.actor_id, CONCAT(a.first_name, ' ', a.last_name) AS name, COUNT(DISTINCT fa.film_id) AS films_count
+        FROM actor AS a
+        JOIN film_actor fa ON fa.actor_id = a.actor_id
+        GROUP BY a.actor_id, name
+        ORDER BY films_count DESC, name ASC
+        LIMIT 5;
     """)
     with db.engine.connect() as conn: #connecting to the db
         rows = conn.execute(sql).mappings().all() #executing the query
@@ -58,37 +58,64 @@ def top5_actors():
 
 #As a user I want to be able to click on any of the top 5 films and view its details
 @app.get("/api/films/<int:film_id>") #using the end of the URL as the film's ID
-def film_details(film_id):
+def film_details(film_id): #function for getting a film's information
     sql = text("""
-               -- getting film information
-               SELECT f.film_id AS id, f.title, f.description, f.release_year, GROUP_CONCAT(DISTINCT c.name ORDER BY c.name SEPARATOR ', ') AS categories,
-               l.name AS language, f.rental_duration, f.rental_rate, f.length AS duration, f.replacement_cost, f.rating, f.special_features, f.last_update,
-               (SELECT COUNT(*)
-               FROM rental AS r
-               JOIN inventory i ON i.inventory_id = r.inventory_id
-               WHERE i.film_id = f.film_id) AS rentals_count
-               FROM film AS f
-               JOIN language AS l ON l.language_id = f.language_id
-               LEFT JOIN film_category AS fc ON fc.film_id   = f.film_id
-               LEFT JOIN category AS c ON c.category_id = fc.category_id
-               WHERE f.film_id = :film_id -- film_id from URL
-               GROUP BY
-               f.film_id, f.title, f.description, f.release_year, l.name, f.rental_duration, f.rental_rate, f.length, f.replacement_cost, f.rating, f.special_features, f.last_update;
-
+        -- getting film information
+        SELECT f.film_id AS id, f.title, f.description, f.release_year, GROUP_CONCAT(DISTINCT c.name ORDER BY c.name SEPARATOR ', ') AS categories,
+        l.name AS language, f.rental_duration, f.rental_rate, f.length AS duration, f.replacement_cost, f.rating, f.special_features, f.last_update,
+        (SELECT COUNT(*)
+        FROM rental AS r
+        JOIN inventory i ON i.inventory_id = r.inventory_id
+        WHERE i.film_id = f.film_id) AS rentals_count
+        FROM film AS f
+        JOIN language AS l ON l.language_id = f.language_id
+        LEFT JOIN film_category AS fc ON fc.film_id   = f.film_id
+        LEFT JOIN category AS c ON c.category_id = fc.category_id
+        WHERE f.film_id = :film_id -- film_id from URL
+        GROUP BY
+        f.film_id, f.title, f.description, f.release_year, l.name, f.rental_duration, f.rental_rate, f.length, f.replacement_cost, f.rating, f.special_features, f.last_update;
     """)
     actors_sql = text("""
-               -- getting actor information to display actors involved when clicking on a film
-               SELECT a.actor_id, CONCAT(a.first_name, ' ', a.last_name) AS name
-               FROM actor AS a
-               JOIN film_actor fa ON fa.actor_id = a.actor_id
-               WHERE fa.film_id = :film_id -- film_id from URL
-               ORDER BY name;
+        -- getting actor information to display actors involved when clicking on a film
+        SELECT a.actor_id, CONCAT(a.first_name, ' ', a.last_name) AS name
+        FROM actor AS a
+        JOIN film_actor fa ON fa.actor_id = a.actor_id
+        WHERE fa.film_id = :film_id -- film_id from URL
+        ORDER BY name;
     """)
     with db.engine.connect() as conn: #connecting to the db
         film = conn.execute(sql, {"film_id": film_id}).mappings().first() #executing query with film_id from URL as parameter
-        actors = conn.execute(actors_sql, {"film_id": film_id}).mappings().all()
+        actors = conn.execute(actors_sql, {"film_id": film_id}).mappings().all() #executing query with film_id from URL as parameter
     data = dict(film) #getting the rows and putting them into a dictionary
-    data["actors"] = [dict(a) for a in actors] #adding the actors to the dictionary as well
+    data["actors"] = [dict(r) for r in actors] #adding the actors to the dictionary as well
+    return jsonify(data) #converting the data found into JSON format
+
+#As a user I want to be able to view the actor’s details and view their top 5 rented films
+@app.get("/api/actors/<int:actor_id>") #using the end of the URL as the actor's ID
+def actor_details(actor_id): #function for getting an actor's information
+    info_sql = text("""  
+        SELECT a.actor_id, CONCAT(a.first_name, ' ', a.last_name) AS name, COUNT(DISTINCT fa.film_id) AS films_count
+        FROM actor AS a
+        LEFT JOIN film_actor AS fa ON fa.actor_id = a.actor_id
+        WHERE a.actor_id = :actor_id -- actor_id from URL
+        GROUP BY a.actor_id, name;
+    """)
+    top5_films_sql = text("""
+        SELECT f.film_id, f.title, COUNT(r.rental_id) AS rentals_count
+        FROM film AS f
+        JOIN film_actor AS fa ON fa.film_id = f.film_id
+        JOIN inventory AS i ON i.film_id = f.film_id
+        JOIN rental AS r ON r.inventory_id = i.inventory_id
+        WHERE fa.actor_id = :actor_id -- actor_id from URL
+        GROUP BY f.film_id, f.title
+        ORDER BY rentals_count DESC
+        LIMIT 5;
+    """)
+    with db.engine.connect() as conn: #connecting to the db
+        info = conn.execute(info_sql, {"actor_id": actor_id}).mappings().first() #executing query with actor_id from URL as parameter
+        top_films = conn.execute(top5_films_sql, {"actor_id": actor_id}).mappings().all() #executing query with actor_id from URL as parameter
+    data = dict(info) #getting the rows and putting them into a dictionary
+    data["top_films"] = [dict(r) for r in top_films] #adding the top films to the dictionary as well
     return jsonify(data) #converting the data found into JSON format
 
 if __name__ == "__main__":
